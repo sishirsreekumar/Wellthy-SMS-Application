bc174575129b871e2d8e06a04b6973f322ab0af0
unknown <sishirsreekumar@gmail.com>
2020-09-15 03:53:28 +0530

    Init commit

--- src/basic_node.js
+++ src/basic_node.js
@@ -1,8 +1,8 @@
 var express = require('express')
-var rr2 = require('request');
+var request = require('request');
 
 var app =express()
-
+const port = process.env.PORT || 3000
 // Parse JSON bodies (as sent by API clients)
 
 app.use(express.urlencoded());
@@ -17,34 +17,34 @@ app.get("/",(req,res)=>{
 
 app.post("/",(req,res)=>{
     res.send("Hello there")
-    // res.send(req)
     console.log(req.body)
 
-    console.log("new bodddy",req.body['lead_mobile_number'])
+    console.log("new body data",req.body.lead_mobile_number)
     console.log("new bodddy",typeof(req.body["lead_mobile_number"]))
     var phone=req.body["lead_mobile_number"]
     var message="You have missed a call.Give a call to this number to schedule an appointment."
     var options = {
         url:"https://api-alerts.kaleyra.com/v4/?api_key=A3cab820f8de4a0f03fb3ea65e50b62a0&method=sms&message="+message+"&to="+phone+"&sender=Welthy",
+    
     };
     
     function callback(error, response, body) {
 
         if(error){
             console.log("ERRROR ",error)
+            res.send("ERROR")
         }
         if (!error && response.statusCode == 200) {
             console.log(body);
         }
     }
     
-    rr2(options, callback);
+    request(options, callback);
 
 
 
 })
-app.listen(process.env.PORT || 5000,()=>{
 
-    console.log("site is live")
-
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
 })

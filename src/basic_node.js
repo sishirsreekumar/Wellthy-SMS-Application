5017491c8dcfc9048a07ab53ba5931b89c28d37f
unknown <sishirsreekumar@gmail.com>
2020-10-05 04:14:04 +0530

    Added console debugging

--- src/basic_node.js
+++ src/basic_node.js
@@ -19,13 +19,13 @@ app.post("/",(req,res)=>{
     res.send("Hello there")
     console.log(req.body)
     console.log("new body data",req.body.lead_mobile_number)
-    console.log("new bodddy",typeof(req.body["lead_mobile_number"]))
     var phone=req.body["lead_mobile_number"]
-    var n = req.body["lead_cf_number_of_times_called"]
+    //var n = req.body["lead_cf_number_of_times_called"]
     var rnr = req.body["lead_cf_number_of_times_rnr"]
     var cb = req.body["lead_cf_number_of_time_call_back"]
     var call_status = req.body["lead_cf_onboarding_remarks"]
     if (call_status = "Ringing"){
+        console.log("Inside 1st if block ")
         if (rnr = 1){
             var message="You seem to be missing out on a lot of action. But I can help you catch up! Tap https://wellthy.page.link/XXEk to connect over Health Coach chat."
             var campaign = "RNR1"
@@ -36,23 +36,24 @@ app.post("/",(req,res)=>{
             var message = "You're one step away from getting your meals and activities in order. Tap https://wellthy.page.link/XXEk to chat with your Health Coach now! "
             var campaign  = "RNR3"
         } else if (rnr=4) {
-            var messsage = "Did you know cinnamon could help keep your blood sugar in control? Tap https://wellthy.page.link/XXEk to talk to your Health Coach for more tips!"
+            var message = "Did you know cinnamon could help keep your blood sugar in control? Tap https://wellthy.page.link/XXEk to talk to your Health Coach for more tips!"
             var campaign = "RNR4"
         } else if (rnr=5) {
-            var messsage = "Can't talk right now? Let's chat! Tap https://wellthy.page.link/XXEk to connect with your Health Coach on chat."
+            var message = "Can't talk right now? Let's chat! Tap https://wellthy.page.link/XXEk to connect with your Health Coach on chat."
             var campaign = "RNR5"
         } else if (rnr=6) {
-            var messsage = "Your health coach wants to talk to you about which exercises help improve blood sugar.Tap https://wellthy.page.link/XXEk to connect over chat."
+            var message = "Your health coach wants to talk to you about which exercises help improve blood sugar.Tap https://wellthy.page.link/XXEk to connect over chat."
             var campaign = "RNR6"
         } else if (rnr=7) {
-            var messsage = "There's so much to know about managing your condition! Tap https://wellthy.page.link/XXEk to have a quick chat with your Health Coach. "
+            var message = "There's so much to know about managing your condition! Tap https://wellthy.page.link/XXEk to have a quick chat with your Health Coach. "
             var campaign = "RNR7"
         }
     }
 
     if (call_status = "Call Back"){
+        console.log("Inside 2nd if block ")
         if (cb = 1){
-            var message="Until we speak again, how about connecting over chat. Tap https://wellthy.page.link/XXEk to chat with your Health Coach & get interesting insights!"
+            var message = "Until we speak again, how about connecting over chat. Tap https://wellthy.page.link/XXEk to chat with your Health Coach & get interesting insights!"
             var campaign = "CallBack1"
         } else if (cb = 2) {
             var message = "It's been a while since we last spoke. Until we speak again, how about a quick chat. Tap https://wellthy.page.link/XXEk to connect."
@@ -62,6 +63,10 @@ app.post("/",(req,res)=>{
             var campaign  = "CallBack3"
         }
     }
+    console.log("TO Remarks: ",req.body.lead_cf_onboarding_remarks)
+    console.log("RNR Number: ",req.body.lead_cf_number_of_times_rnr)
+    console.log("CallBack Number ",req.body.lead_cf_number_of_time_call_back)
+    console.log("Campaign: ", campaign)
     var options = {
         url:"https://api-alerts.kaleyra.com/v4/?api_key=A3cab820f8de4a0f03fb3ea65e50b62a0&method=sms&message="+message+"&to="+phone+"&sender=Welthy&campaign="+campaign,
     
